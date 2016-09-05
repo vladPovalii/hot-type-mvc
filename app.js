34d3c4f16fe9b1e9d538767a53286d80d4f89c99
Vlad Povalii <vladpovalii@gmail.com>
2016-09-05 13:41:45 +0300

    add config for connection info

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// get object with connection information 
+var env = process.env.NODE_ENV || 'development';
+var config = require('./config')[env];
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
