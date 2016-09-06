17fe9b34b7d77de94af6e8178d7da5c6513bbf23
VladPovalii <vladpovalii@gmail.com>
2016-09-07 02:18:09 +0300

    collection fetch test

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,13 @@ var header = document.querySelector("h1");
 header.innerHTML = "atata";
 
 
+var TargetModel = Backbone.Model.extend({});
+
+var TargetCollection = Backbone.Collection.extend({
+	model: TargetModel,
+	url: 'http://localhost:3000/api/targets'
+});
+
+var targetCollection = new TargetCollection;
+targetCollection.fetch();
+console.log(TargetCollection.length);
