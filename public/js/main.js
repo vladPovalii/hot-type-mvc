b599b94b9a079d5fad7232e258c9a2702c325789
Vlad Povalii <vladpovalii@gmail.com>
2016-09-13 18:14:18 +0300

    add requirejs.config

--- public/js/main.js
+++ public/js/main.js
@@ -1,87 +1,106 @@
-
-/* for future sync with server
-Backbone.Model.prototype.idAttribute = '_id';
-var TargetModel = Backbone.Model.extend({});
-
-var TargetCollection = Backbone.Collection.extend({
-	url: 'http://localhost:3000/api/targets'
-});
-
-var targetCollection = new TargetCollection;
-targetCollection.fetch({
-	success: function(response) {
-		_.each(response.toJSON(), function(item) {
-			console.log('Successfully GOT blog with _id: ' + item._id);
-		})
+require.config({
+	paths: {
+		"jquery": "libs/jquery/jquery",
+		"underscore": "libs/underscore/underscore",
+		"backbone": "libs/backbone/backbone"
 	},
-	error: function() {
-		console.log('Failed to get blogs!');
+	shim: {
+		"underscore": {
+			exports: "_"
+		},
+		"backbone": {
+			deps: ["underscore", "jquery"],
+			exports: "Backbone"
+		}
 	}
 });
-*/
 
-document.addEventListener("DOMContentLoaded", ready);
+require(["jquery", "underscore", "backbone"], function($, _, Backbone){
 
-function ready(){
+		/* for future sync with server
+		Backbone.Model.prototype.idAttribute = '_id';
+		var TargetModel = Backbone.Model.extend({});
 
-	var FIRST_TYPE = true;
+		var TargetCollection = Backbone.Collection.extend({
+			url: 'http://localhost:3000/api/targets'
+		});
 
-	var TypeModel = Backbone.Model.extend({});
-
-	var TypeView = Backbone.View.extend({
-		
-		tagName: "div",
-
-		tpl: _.template($('#item-template').html()),
-		
-		className: "type-item",
-
-		getTarget: function(){
-			return $("#target_input")[0].value;
-		},
-
-		initialize: function(){
-			this.render();
-		},
-
-		inputCheck: function(type_str) {
-			if(type_str === this.getTarget()){
-				return true;
-			}else{
-				return false;
+		var targetCollection = new TargetCollection;
+		targetCollection.fetch({
+			success: function(response) {
+				_.each(response.toJSON(), function(item) {
+					console.log('Successfully GOT blog with _id: ' + item._id);
+				})
+			},
+			error: function() {
+				console.log('Failed to get blogs!');
 			}
-		},
+		});
+		*/
 
-		render: function(){
-			var type_str = this.model.get("value")
-			var type_list = document.querySelector(".type-list");
-			this.$el.html(this.tpl({value: type_str}));
-			if(!this.inputCheck(type_str)){
-				this.el.classList.add("type-item--striked");
-			}
-			if(FIRST_TYPE){
-				type_list.appendChild(this.el);	
-				FIRST_TYPE = false;		
-			}else{
-				type_list.insertBefore(this.el, type_list.childNodes[0]);
-			}
-			return this;
-		}
-	});
+		document.addEventListener("DOMContentLoaded", ready);
 
-	$("#target_form").submit(function(event) {
-		event.preventDefault();
-	});
-	
-	$("#type_form").submit(function(event) {
-		var submit_str = event.target[0].value;
-		if(submit_str !== ""){
-				var word = new TypeModel({"value" : submit_str});
-				var wordView = new TypeView({"model": word});
-				event.target[0].value = "";
+		function ready(){
+
+			var FIRST_TYPE = true;
+
+			var TypeModel = Backbone.Model.extend({});
+
+			var TypeView = Backbone.View.extend({
+				
+				tagName: "div",
+
+				tpl: _.template($('#item-template').html()),
+				
+				className: "type-item",
+
+				getTarget: function(){
+					return $("#target_input")[0].value;
+				},
+
+				initialize: function(){
+					this.render();
+				},
+
+				inputCheck: function(type_str) {
+					if(type_str === this.getTarget()){
+						return true;
+					}else{
+						return false;
+					}
+				},
+
+				render: function(){
+					var type_str = this.model.get("value")
+					var type_list = document.querySelector(".type-list");
+					this.$el.html(this.tpl({value: type_str}));
+					if(!this.inputCheck(type_str)){
+						this.el.classList.add("type-item--striked");
+					}
+					if(FIRST_TYPE){
+						type_list.appendChild(this.el);	
+						FIRST_TYPE = false;		
+					}else{
+						type_list.insertBefore(this.el, type_list.childNodes[0]);
+					}
+					return this;
+				}
+			});
+
+			$("#target_form").submit(function(event) {
 				event.preventDefault();
-		}else{
-			event.preventDefault();
+			});
+			
+			$("#type_form").submit(function(event) {
+				var submit_str = event.target[0].value;
+				if(submit_str !== ""){
+						var word = new TypeModel({"value" : submit_str});
+						var wordView = new TypeView({"model": word});
+						event.target[0].value = "";
+						event.preventDefault();
+				}else{
+					event.preventDefault();
+				}
+			});
 		}
-	});
-}
+});
